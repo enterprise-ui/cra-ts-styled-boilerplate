103f0f61185c29c1b80ff49f99001af38312a749
FeHHix <aisaenok@gmail.com>
2020-05-14 18:44:09 +0300

    Restored App SPA.

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,11 +1,40 @@
+import './i18n';
+
 import React from 'react';
 
-import { AppContainerClient } from 'cra-ts-styled-boilerplate-pages';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
 
+import { usePrefersDarkMode } from './hooks/usePrefersDarkMode';
+import { App } from './main/App';
+import configureStore from './store/configureStore';
+import { GlobalStyle } from './styles/global';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppContainerClient />, document.getElementById('root'));
+const AppContainer = () => {
+  const prefersDarkMode = usePrefersDarkMode();
+  const { store, persistor } = configureStore();
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={{ mode: prefersDarkMode ? 'dark' : 'light' }}>
+            <GlobalStyle />
+            <Router>
+              <App />
+            </Router>
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
